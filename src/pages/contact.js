231e91e91e5fa47046268c7ed7cb1a8cbb02f159
marian brinza <branza.marian03@yahoo.com>
2020-04-11 22:17:15 +0300

    add about, blog and contact pages

---
+++ src/pages/contact.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Layout from '../components/layout/layout';
+
+const ContactPage = () => {
+  return (
+    <Layout>
+      <h1>Contact me at the following numbers</h1>
+      <ul>
+        <li>Phone: 555-555-357</li>
+        <li>E-mail: youwish@something.com</li>
+        <li>Address: somewhere or nowhere</li>
+      </ul>
+
+      <p>View this tutorial on <a href="https://www.youtube.com/watch?v=8t0vNu2fCCM&t=1420s" target="_blank">Youtube</a>
+      </p>
+    </Layout>
+  );
+};
+
+export default ContactPage;
