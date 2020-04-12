64645ba6b7a533d552f523949b9f9d34e39e15b8
marian brinza <branza.marian03@yahoo.com>
2020-04-12 19:10:04 +0300

    add siteMetadata obj

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,10 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata:{
+    title: 'Learning Gatsby with Andrew Mead',
+    author: 'Marian Burtoaca'
+  },
   plugins: [
     // plugin name
     'gatsby-plugin-sass'
