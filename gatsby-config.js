f1dfef25d170cff70e204caa3aef46225ac01614
marian brinza <branza.marian03@yahoo.com>
2020-04-13 23:14:13 +0300

    add gatsby-node.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,15 +2,30 @@
  * Configure your Gatsby site with this file.
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ *******************************************
+ *   REMEMBER THIS IS A NODE.JS FILE !!
+ *******************************************
  */
 
 module.exports = {
-  siteMetadata:{
+  siteMetadata: {
     title: 'Learning Gatsby with Andrew Mead',
-    author: 'Marian Burtoaca'
+    author: 'Marian Burtoaca',
   },
   plugins: [
     // plugin name
-    'gatsby-plugin-sass'
-  ]
-}
+    'gatsby-plugin-sass',
+    {
+      // this plugin loads the files from file system
+      // resolve property is the name of the plugin
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        // path to where our content is (posts folder in our case)
+        path: `${__dirname}/src/posts/`,
+      },
+      // this plugin looks for md files and parses them
+    },
+    'gatsby-transformer-remark',
+  ],
+};
